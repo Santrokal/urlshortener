1d597cbc66dbe3bf782f746522e0a8beb605ee5a
SantroKal <msalman90826@gmail.com>
2025-06-19 16:42:09 +0530

    commit

--- index.js
+++ index.js
@@ -2,16 +2,32 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/shorturl', function(req, res) {
+  const originalUrl = req.body.url;
+  const id = Math.floor(10000 + Math.random() * 90000); // random 5-digit number
+  res.json({ original_url: originalUrl, short_url: id });
+})
+
+// Redirect route
+app.get('/api/shorturl/:id', (req, res) => {
+
+    res.json({ error: "No short URL found for given input" })
 });
 
 // Your first API endpoint
